5466c2d89521185e1749166940392810ea713852
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-04-05 13:00:47 +0700

    up

--- NumberOfPods.py
+++ NumberOfPods.py
@@ -2,11 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('Soybean.csv', usecols= ['Number of Pods (NP)'])
+df = pd.read_csv('Soybean.csv', usecols= ['Number of Pods (NP)','Parameters'])
 pods = df['Number of Pods (NP)']
+param = df['Parameters']
 
 # Contoh analisis dasar
 print(pods.describe())
+print(df.corr())
 # mencoba mencari Z-score
 Zscore = (df['Number of Pods (NP)'] - df['Number of Pods (NP)'].mean()) / df['Number of Pods (NP)'].std()
 print("Z-score =")
@@ -16,11 +18,13 @@ print(Zscore)
 plt.figure(figsize=(10,6))
 sns.histplot(pods, kde=True, bins=30, color='crimson')
 
-# Tambahkan elemen visual
+'''# Tambahkan elemen visual
+sns.scatterplot(x=param, y=pods)
 plt.title('Distribusi Persebaran Kedelai berdasar Varietas')
-plt.xlabel('Jumlah ')
+plt.xlabel('Varietas')
 plt.ylabel('Frekuensi')
 plt.grid(True)
 
 # Tampilkan plot
 plt.show()
+'''
