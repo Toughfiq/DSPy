3c665d5e561cde21bfd9e5490c2f0ca70b58fc8f
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-04-05 12:01:59 +0700

    NP update

--- NumberOfPods.py
+++ NumberOfPods.py
@@ -7,3 +7,20 @@ pods = df['Number of Pods (NP)']
 
 # Contoh analisis dasar
 print(pods.describe())
+# mencoba mencari Z-score
+Zscore = (df['Number of Pods (NP)'] - df['Number of Pods (NP)'].mean()) / df['Number of Pods (NP)'].std()
+print("Z-score =")
+print(Zscore)
+
+# Buat plot distribusi
+plt.figure(figsize=(10,6))
+sns.histplot(pods, kde=True, bins=30, color='crimson')
+
+# Tambahkan elemen visual
+plt.title('Distribusi Persebaran Kedelai berdasar Varietas')
+plt.xlabel('Jumlah ')
+plt.ylabel('Frekuensi')
+plt.grid(True)
+
+# Tampilkan plot
+plt.show()
