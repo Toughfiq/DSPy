0ef3d102db45d036004c6a8af21b02fb22415405
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-04-05 13:09:02 +0700

    update NumberOfPods.py

--- NumberOfPods.py
+++ NumberOfPods.py
@@ -2,29 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('Soybean.csv', usecols= ['Number of Pods (NP)','Parameters'])
+# Load data dengan nama kolom yang benar
+df = pd.read_csv('Soybean.csv', usecols=['Number of Pods (NP', 'Parameters'])
+
 pods = df['Number of Pods (NP)']
 param = df['Parameters']
 
-# Contoh analisis dasar
+# Analisis dasar
+print("Deskripsi Pods:")
 print(pods.describe())
+
+print("\nKorelasi:")
 print(df.corr())
-# mencoba mencari Z-score
-Zscore = (df['Number of Pods (NP)'] - df['Number of Pods (NP)'].mean()) / df['Number of Pods (NP)'].std()
-print("Z-score =")
+
+# Z-score
+Zscore = (pods - pods.mean()) / pods.std()
+print("\nZ-score:")
 print(Zscore)
 
-# Buat plot distribusi
+# Plot histogram
 plt.figure(figsize=(10,6))
 sns.histplot(pods, kde=True, bins=30, color='crimson')
 
-'''# Tambahkan elemen visual
+# Scatter plot
 sns.scatterplot(x=param, y=pods)
-plt.title('Distribusi Persebaran Kedelai berdasar Varietas')
-plt.xlabel('Varietas')
-plt.ylabel('Frekuensi')
+plt.title('Scatter Plot Parameter vs Jumlah Polong')
+plt.xlabel('Parameter')
+plt.ylabel('Jumlah Polong')
 plt.grid(True)
 
-# Tampilkan plot
+# Tampilkan
 plt.show()
-'''
