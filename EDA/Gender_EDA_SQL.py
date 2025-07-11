6e6898f8c37b10b6642f1a7546393f0e9ae4a792
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-07-12 00:59:45 +0700

    Menambah bar chart

--- EDA/Gender_EDA_SQL.py
+++ EDA/Gender_EDA_SQL.py
@@ -41,3 +41,25 @@ gender_percent = gender_df['gender'].value_counts(normalize=True) * 100
 print("Unique values:", gender_df['gender'].unique()) #mengambil unique value setelah pembersihan
 print("\nCounts:\n", gender_counts) #menghitung frekuensi tiap gender yang ada
 print("\nPercentages:\n", gender_percent.round(2)) #menampilkan presentase dengan pembulatan 2 desimal
+
+# 4. Plot bar chart dari data yang sudah dihitung diatas
+plt.figure(figsize=(10, 6)) # membuat canvas kosong untuk plot dengan ukuran 10x6 inch
+#menambahkan bar chart
+ax = sns.countplot(data=gender_df, x='gender', order=gender_counts.index, palette='pastel') #countplot akan menghitung setiap kategori count
+#x = 'gender' menunjukkan kolom yang dijadikan sumbu x
+#order= gender_counts.index akan mengurutkan jumlah terbanyak berada paling kiri
+
+# Menambah label angka total counts diatas tiap bar
+for p in ax.patches:
+    ax.annotate(f'{int(p.get_height())}', 
+                (p.get_x() + p.get_width() / 2., p.get_height()), 
+                ha='center', va='center', 
+                xytext=(0, 5), 
+                textcoords='offset points')
+
+plt.title('Distribution of Gender Categories', fontsize=15) #judul grafik
+plt.xlabel('Gender', fontsize=12) #judul sumbu x
+plt.ylabel('Count', fontsize=12)#judul sumbu y
+plt.xticks(rotation=45) #memiringkan label sumbu x aagr terbaca dan tidak bertumpuk
+plt.tight_layout() # digunakan untuk menyesuaikan padding agar tidak terpotong
+plt.show() #menampilkan hasil plot
