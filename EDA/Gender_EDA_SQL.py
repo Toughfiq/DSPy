53e722eceeb564d4cc97a518e7b735b4fb73f82f
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-07-12 00:44:42 +0700

    Menambahkan beberapa analis sederhana sementara

--- EDA/Gender_EDA_SQL.py
+++ EDA/Gender_EDA_SQL.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import mysql.connector
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.db_connection import load_column, load_columns
 
 #Load data menggunakan database config
 db_config = {
@@ -10,22 +14,30 @@ db_config = {
     'password': 'Databases123',
     'database': 'DatingSQL'
 }
-#Membuat handling exception
-try:
-    conn = mysql.connector.connect(**db_config)
-    
-    # Mengubah query untuk mengambil SEMUA kolom
-    sql_query = "SELECT * FROM user_data" 
-    
-    # Menggunakan pd.read_sql_query untuk memuat data langsung ke DataFrame
-    data_df = pd.read_sql_query(sql_query, conn) #data_df sudah menjadi sebuah dataframe di pandas
-    
-    print("All data successfully loaded from MySQL.")
+gender_df = load_column('gender')
+#Memulai pembersihan data
+def Gender_Def (value):
+    if pd.isna(value): #Fungsi pd.isna() mengecek satu nilai (value) apakah NaN atau tidak.
+        return 'Unknown'
+    value = str(value).strip()  # Mengganti spasi atau white space menjadi type string agar terbaca
+    if value.lower() == 'female':
+        return 'Female'
+    elif value.lower() == 'male':
+        return 'Male'
+    elif value.lower() == 'genderfluid':
+        return 'Genderfluid'
+    elif value.lower() in ['non-binary', 'nonbinary']: #mencegah adanya kesalah menulis
+        return 'Non-binary'
+    elif value.lower() in ['prefer not to say', 'prefer not to answer']:
+        return 'Prefer Not to Say'
+    else:
+        return value.title()  # Kapitalisasi respon lain  
+gender_df['gender'] = gender_df['gender'].apply(Gender_Def) #inisiasi data yang sudah dibersihkan kembali ke gender_df
 
-except mysql.connector.Error as err:
-    print(f"Error connecting to MySQL or executing query: {err}")
-    exit() # Keluar dari skrip jika gagal terhubung ke DB
-finally:
-    if 'conn' in locals() and conn.is_connected():
-        conn.close()
-        print("MySQL connection closed.")
+# 3.perhitungan data
+gender_counts = gender_df['gender'].value_counts()
+gender_percent = gender_df['gender'].value_counts(normalize=True) * 100
+
+print("Unique values:", gender_df['gender'].unique()) #mengambil unique value setelah pembersihan
+print("\nCounts:\n", gender_counts) #menghitung frekuensi tiap gender yang ada
+print("\nPercentages:\n", gender_percent.round(2)) #menampilkan presentase dengan pembulatan 2 desimal
