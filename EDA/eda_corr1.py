c37a9eba999bc1b5476bd1fc310422eb94ba1575
Muhammad Taufiq Rizky <127406993+Toughfiq@users.noreply.github.com>
2025-07-12 03:57:43 +0700

    Menambahkan sebuah file correlation

---
+++ EDA/eda_corr1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import mysql.connector
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.db_connection import load_column, load_columns
+#Ambil config Databases dulu
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'Databases123',
+    'database': 'DatingSQL'
+}
+eda_corr_df = load_columns(['gender', 'sexual_orientation'])
